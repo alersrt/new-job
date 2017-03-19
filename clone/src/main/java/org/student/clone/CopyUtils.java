4936afb0528f1982225ae183846e5317252e068d
alersrt <strtmg@yandex.ru>
2017-03-20 03:12:27 +0700

    Refactoring. Change of implemented interface of inner class from Runnable to Callable.

---
+++ clone/src/main/java/org/student/clone/CopyUtils.java
@@ -0,0 +1,17 @@
+package org.student.clone;
+
+/**
+ * It is util class which provide functions of object
+ * copying
+ */
+public class CopyUtils {
+	/**
+	 * Return copies of object
+	 * @param obj the copied object
+	 * @param <T> type of copied object
+	 * @return {@code T objClone} clone of copied object
+	 */
+	public <T> T deepClone(T obj) {
+		return null;
+	}
+}
