6a21e616fb4368ef1272604439735d3d5d5fdf76
alersrt <strtmg@yandex.ru>
2017-03-22 23:29:01 +0700

    Create util for deep cloning

--- clone/src/main/java/org/student/clone/CopyUtils.java
+++ clone/src/main/java/org/student/clone/CopyUtils.java
@@ -1,17 +1,38 @@
 package org.student.clone;
 
+import java.io.*;
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * It is util class which provide functions of object
- * copying
+ * copying. It works with serializable objects
  */
 public class CopyUtils {
 	/**
-	 * Return copies of object
+	 * Return copies of object.
 	 * @param obj the copied object
 	 * @param <T> type of copied object
-	 * @return {@code T objClone} clone of copied object
+	 * @return {@code (T) object} clone of copied object
 	 */
-	public <T> T deepClone(T obj) {
-		return null;
+	public static <T> T deepClone(T obj) throws IOException, ClassNotFoundException {
+		// Serializing input object in to byte stream
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+		objectOutputStream.writeObject(obj);
+
+		// Reading object from byte stream
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+		ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+
+		// Closing streams
+		byteArrayInputStream.close();
+		byteArrayOutputStream.close();
+		objectInputStream.close();
+		objectOutputStream.close();
+
+		return (T) objectInputStream.readObject();
 	}
+
 }
